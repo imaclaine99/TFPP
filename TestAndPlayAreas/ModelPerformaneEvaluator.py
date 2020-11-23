8e62ed7822e2425c76fb082e3f1a0043bd4c12a7
Ivan <imaclaine99@gmail.com>
2020-11-23 22:11:53 +1100

    Playpen code to show predicted values per epoch.  Useful for debugging

--- TestAndPlayAreas/ModelPerformaneEvaluator.py
+++ TestAndPlayAreas/ModelPerformaneEvaluator.py
@@ -1,11 +1,15 @@
 import MyLSTMModelV2b
 import ModelV2Config as ModelV2Config
 import MyFunctions as myf
+import numpy as np
+
+
+myf.clear_session = False   # Set this to False, so we can keep the model for re-use.
 
 output_col = 'BuyWeightingRule'
 
 # Load a model
-model_for_prediction = 'BuyV3_22777_3_Layers_BuyV3.h5'    #'31204_5_Layers.h5'
+model_for_prediction = 'BuyV3b_250_22776_2_Layers_BuyV3b.h5'    #'31204_5_Layers.h5'
 model = myf.load_model(model_for_prediction)
 print('')
 print('[INFO] Model File: ' + model_for_prediction)
@@ -43,18 +47,46 @@ standardised_loss = results[0]  # Is this correct?  Seems like it should be
 # Function should NOT care about model or data being correct - that is up to the caller.
 # Maybe output both the actual loss as well as some form of box plot of the loss - would be useful to see not just the 'average' but the spread.
 
+## Best Guess Logic on xtrain, ytrain
+# Let's assume one ytrain value
+if len(ytrain_new[0]) != 1:
+    print('[ERROR] Multile output values are not yet handled')
 
-# Let's run another fit cycle - see if it gets better / worse
-myf.parse_process_plot(parsed_filename, output_col, model.model, 'Reprocess Testing' )
 
-# Standardised Loss, as per Standard Process
-xtrain_new, xtest_new, ytrain_new, ytest_new = myf.parse_data_to_trainXY (parsed_filename, output_col, test_size=0.001)
+min_target = min(ytrain_new)[0]
+max_target = max(ytrain_new)[0]
 
-# Test size of 0.1% means that Train is effectively all the data (close enough!!)
+best_loss = 999999
+for i in np.arange(min_target, max_target, 0.01):
+    # Do something
+    loss = np.sum(np.square(ytrain_new-i))/len(ytrain_new)
+    if loss < best_loss:
+        best_loss = loss
+        best_value = i
 
-model.compile(loss=myf.default_model_loss_func, optimizer=myf.default_optimizer,
-              metrics=['accuracy'])  # This feels a bit hacky...
-results = model.evaluate(xtest_new, ytest_new, batch_size=myf.batch_size)
-print(results)
+print (f"Best Guess Loss was: {best_loss} at a value of {best_value}")
 
-standardised_loss = results[0]  # Is this correct?  Seems like it should be
+standardised_loss = []
+
+for i in range(0, 5):
+    # Let's run another fit cycle - see if it gets better / worse
+    myf.parse_process_plot(parsed_filename, output_col, model.model, 'Reprocess Testing' )
+
+    # Standardised Loss, as per Standard Process
+    xtrain_new, xtest_new, ytrain_new, ytest_new = myf.parse_data_to_trainXY (parsed_filename, output_col, test_size=0.001)
+
+    # Test size of 0.1% means that Train is effectively all the data (close enough!!)
+
+    model.compile(loss=myf.default_model_loss_func, optimizer=myf.default_optimizer,
+                  metrics=['accuracy'])  # This feels a bit hacky...
+    results = model.evaluate(xtest_new, ytest_new, batch_size=myf.batch_size)
+    print(results)
+
+    standardised_loss.append(results[0])  # Is this correct?  Seems like it should be
+
+
+print (f'Model {model_for_prediction} and data file {parsed_filename} has been processed')
+print (f'This file has a best guess loss of {best_loss} with a guess value of {best_value}')
+print ('Additional processing gives the following loss values:')
+for i in standardised_loss:
+    print ('\t' + str(i))
